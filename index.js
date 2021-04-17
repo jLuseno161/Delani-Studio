f9990ec2a42e57ec0558561fb59ccb844063c1f0
jLuseno161 <joyluseno2@gmail.com>
2021-04-17 16:50:39 +0300

    hover effect using jQuery

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     //toggle between image and description
     $(".design").click(function () {
         $("#design_image, #design_text").toggle();
@@ -10,4 +10,12 @@ $(document).ready(function () {
     $(".product").click(function () {
         $("#product_image, #product_text").toggle();
     })
+
+
+    $(".portfolio .col-md-3 img").hover(function () {
+        $(this).css("opacity", "0.4");
+    }, function () {
+        $(this).css("opacity", "1");
+    });
+
 });
